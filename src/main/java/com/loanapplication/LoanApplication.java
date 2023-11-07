ae8a06a83d72905baaf3042afb6a5aa290c2ed88
Habay <biodun.banjoko@yahoo.com>
2023-11-07 17:27:58 +0100

    method level security

--- src/main/java/com/loanapplication/LoanApplication.java
+++ src/main/java/com/loanapplication/LoanApplication.java
@@ -2,8 +2,10 @@ package com.loanapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true,  securedEnabled = true,  jsr250Enabled = true)
 public class LoanApplication {
 
 	public static void main(String[] args) {
